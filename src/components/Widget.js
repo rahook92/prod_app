24f9635a3f2d4eb03f1a1056dd3ebef25b9785d3
rahook92 <rahook92@gmail.com>
2020-02-11 17:51:37 -0500

    first push to prod-app

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -4,64 +4,7 @@ import GoalForm from './GoalForm';
 
 class Widget extends React.Component {
     state = {
-        goals: [
-            {
-                id: 1,
-                title: 'Get a Web-Dev Job',
-                completed: false,
-                tasks: [
-                    {
-                        id: 1,
-                        title: 'Learn React',
-                        completed: false
-                    },
-                    {
-                        id: 2,
-                        title: 'Create a portfolio',
-                        completed: false
-                    },
-                    {
-                        id: 3,
-                        title: 'Apply to tech companies',
-                        completed: false
-                    }
-                ]
-            },
-            {
-                id: 2,
-                title: 'Decrease running mile average',
-                completed: false,
-                tasks: [
-                    {
-                        id: 1,
-                        title: 'HIIT training',
-                        completed: false
-                    },
-                    {
-                        id: 2,
-                        title: 'stretch more',
-                        completed: false
-                    }
-                ]
-            },
-            {
-                id: 3,
-                title: 'Meditate for an hour straight',
-                completed: false,
-                tasks: [
-                    {
-                        id: 1,
-                        title: 'meditate for 30 minutes',
-                        completed: false
-                    },
-                    {
-                        id: 2,
-                        title: 'meditate for 45 minutes',
-                        completed: false
-                    }
-                ]
-            }
-        ]
+        goals: []
     }
 
     toggleSelect = (e) => {
